04787313d3bf95eb3134cd8b625d163eec7ecad9
Skullbite <mawler12@gmail.com>
2018-04-27 16:57:03 -0400

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -187,7 +187,7 @@ class Moderator:
 
         if ctx.invoked_subcommand is None:
             help_cmd = self.bot.get_command('help')
-            await ctx.invoke(help_cmd, 'remove')
+            await ctx.invoke(help_cmd, 'prune')
 
     async def do_removal(self, ctx, limit, predicate, *, before=None, after=None, message=True):
         if limit > 2000:
