d00e34fa2c8cd392269321f3cf6a327ac8651fb5
Skullbite <mawler12@gmail.com>
2018-05-01 12:50:45 -0400

    Update index.py

--- index.py
+++ index.py
@@ -11,16 +11,9 @@ Made by AlexFlipnote
 """
 
 
-class HelpFormat(HelpFormatter):
-    async def format_help_for(self, context, command_or_bot):
-        if permissions.can_react(context):
-            await context.message.add_reaction(chr(0x2709))
-
-        return await super().format_help_for(context, command_or_bot)
-
 print("-w- zzz...")
-help_attrs = dict(hidden=True)
-bot = Bot(command_prefix=commands.when_mentioned_or("owo "), prefix=commands.when_mentioned_or("owo "), pm_help=True, help_attrs=help_attrs, formatter=HelpFormat())
+
+bot = Bot(command_prefix=commands.when_mentioned_or("owo "), prefix=commands.when_mentioned_or("owo "), pm_help=False)
 
 for file in os.listdir("cogs"):
     if file.endswith(".py"):
