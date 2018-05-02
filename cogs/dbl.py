bd86eebbcd2c12406d01419f633861d6f41ab2eb
Skullbite <mawler12@gmail.com>
2018-05-02 10:19:23 -0400

    Update dbl.py

--- cogs/dbl.py
+++ cogs/dbl.py
@@ -17,7 +17,7 @@ class DiscordBotsOrgAPI:
 
     async def update_stats(self):
         """This function runs every 30 minutes to automatically update your server count"""
-       await self.bot.is_ready()
+        await self.bot.is_ready()
         while not bot.is_closed:
             print('Attempting to post server count')
             try:
