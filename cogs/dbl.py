2ca46926e9e260ce081b5b75665c423f25a71434
Skullbite <mawler12@gmail.com>
2018-05-02 10:15:51 -0400

    hmm

--- cogs/dbl.py
+++ cogs/dbl.py
@@ -1,33 +1,31 @@
-import dbl
 import discord
 from discord.ext import commands
-
+import dbl
 import aiohttp
 import asyncio
 import logging
-import os
-from utils import repo
+
 
 class DiscordBotsOrgAPI:
     """Handles interactions with the discordbots.org API"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.token = os.environ["DBL_TOKEN"]
-        self.dblpy = dbl.Client(self.bot, self.token)
-        
+        self.token = os.environ["DBL_TOKEN"] #  set this to your DBL token
+        self.dblpy = dbl.Client(self.bot, self.token, loop=bot.loop)
+        self.updating = bot.loop.create_task(self.update_stats())
 
-    @commands.command()
-    @commands.cooldown(rate=1, per=1800, type=commands.BucketType.user)
-    @commands.check(repo.is_owner)
-    async def update(self, ctx):
-        """Updates server count on dbl"""
-        try:
-            await self.dblpy.post_server_count()
-            await ctx.send(f"oki my dbl page now shows i'm in {len(ctx.bot.guilds)} guilds ^w^")
-        except Exception as e:
-            print(e)
-            await ctx.send("hmph.. there was an error.. try again later i guess..")
+    async def update_stats(self):
+        """This function runs every 30 minutes to automatically update your server count"""
+       await self.bot.is_ready()
+        while not bot.is_closed:
+            print('Attempting to post server count')
+            try:
+                await self.dblpy.post_server_count()
+                print('Posted server count ({})'.format(len(self.bot.guilds)))
+            except Exception as e:
+                print('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            await asyncio.sleep(1800)
 
 def setup(bot):
     bot.add_cog(DiscordBotsOrgAPI(bot))
