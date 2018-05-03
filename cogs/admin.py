8618277ac92e3f19957e6a54f65573f1da7f6986
Skullbite <mawler12@gmail.com>
2018-05-03 12:51:54 -0400

    eval i guess..

--- cogs/admin.py
+++ cogs/admin.py
@@ -86,5 +86,53 @@ class Admin:
         output = subprocess.check_output(text_parsed).decode()
         await ctx.send(f"```fix\n{output}\n```")
   
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    @commands.check(repo.is_owner) 
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+                
 def setup(bot):
     bot.add_cog(Admin(bot))
