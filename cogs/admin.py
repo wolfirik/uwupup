3a6811ed146858e141fd68b2feea90544700e642
Skullbite <mawler12@gmail.com>
2018-05-08 12:05:17 -0400

    :ok_hand:

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,6 +8,10 @@ import asyncio
 import discord
 from discord import Webhook, AsyncWebhookAdapter
 import aiohttp
+from contextlib import redirect_stdout
+from copy import copy
+import inspect
+import textwrap
 
 async def run_cmd(cmd: str) -> str:
     """Runs a subprocess and returns the output."""
@@ -30,6 +34,32 @@ class Admin:
 
         # remove `foo`
         return content.strip('` \n')
+
+    def get_syntax_error(e):
+        """Format a syntax error to send to the user.
+        Returns a string representation of the error formatted as a codeblock.
+        """
+        if e.text is None:
+            return box('{0.__class__.__name__}: {0}'.format(e), lang="py")
+        return box(
+            '{0.text}{1:>{0.offset}}\n{2}: {0}'
+            ''.format(e, '^', type(e).__name__),
+            lang="py")
+
+
+    def get_pages(msg: str):
+        """Pagify the given message for output to the user."""
+        return pagify(msg, delims=["\n", " "], priority=True, shorten_by=10)
+
+
+    def sanitize_output(ctx: commands.Context, input_: str) -> str:
+        """Hides the bot's token from a string."""
+        token = ctx.bot.http.token
+        r = "[EXPUNGED]"
+        result = input_.replace(token, r)
+        result = result.replace(token.lower(), r)
+        result = result.replace(token.upper(), r)
+        return result
 
     @commands.command()
     async def amiadmin(self, ctx):
@@ -202,11 +232,64 @@ class Admin:
                         key=lambda s: s.name.lower())
         msg = ""
         for i, server in enumerate(guilds, 1):
-            msg += "{}: {}\n".format(i, server.name)
+            msg += "`{}:` {}\n".format(i, server.name)
 
         for page in pagify(msg, ['\n']):
             await ctx.send(page)
-  
+
+    @commands.command()
+    @commands.check(repo.is_owner)
+    async def debug(self, ctx, *, code):
+        """Evaluate a statement of python code.
+        The bot will always respond with the return value of the code.
+        If the return value of the code is a coroutine, it will be awaited,
+        and the result of that will be the bot's response.
+        Note: Only one statement may be evaluated. Using await, yield or
+        similar restricted keywords will result in a syntax error. For multiple
+        lines or asynchronous code, see [p]repl or [p]eval.
+        Environment Variables:
+            ctx      - command invokation context
+            bot      - bot object
+            channel  - the current channel object
+            author   - command author's member object
+            message  - the command's message object
+            discord  - discord.py library
+            commands - discord.py commands extension
+            _        - The result of the last dev command.
+        """
+        env = {
+            'bot': ctx.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            'discord': discord,
+            'commands': commands,
+            '_': self._last_result
+        }
+
+        code = self.cleanup_code(code)
+
+        try:
+            result = eval(code, env)
+        except SyntaxError as e:
+            await ctx.send(self.get_syntax_error(e))
+            return
+        except Exception as e:
+            await ctx.send(
+                box('{}: {!s}'.format(type(e).__name__, e), lang='py'))
+            return
+
+        if asyncio.iscoroutine(result):
+            result = await result
+
+        self._last_result = result
+
+        result = self.sanitize_output(ctx, str(result))
+
+        await ctx.send_interactive(self.get_pages(result), box_lang="py")
+
     @commands.command()
     @commands.check(repo.is_owner)
     async def whtest(self, ctx, whlink: str):
