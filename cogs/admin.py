deeab6d0afb98e1b6fbdb0f3adf2dd98dae03f56
Skullbite <mawler12@gmail.com>
2018-05-08 12:16:12 -0400

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -280,7 +280,7 @@ class Admin:
 
         self._last_result = result
 
-        await ctx.send(f"{self.get_pages(result), box_lang=\"py\"}")
+        await ctx.send(f"{self.get_pages(result)}")
 
     @commands.command()
     @commands.check(repo.is_owner)
