497ba953c574d966bd80ccb7cdb9014fa6c5ddf7
Skullbite <mawler12@gmail.com>
2018-05-08 10:13:50 -0400

    merged the bot management commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,6 +1,6 @@
 import time
 import subprocess
-from utils import repo, default
+from utils import repo, default, dataIO
 from discord.ext import commands
 import os
 import asyncio
@@ -85,6 +85,74 @@ class Admin:
             return
         await ctx.send(f"Unloaded extension **{name}.py**")
 
+    @commands.group()
+    @commands.check(repo.is_owner)
+    async def change(self, ctx):
+        if ctx.invoked_subcommand is None:
+            _help = await ctx.bot.formatter.format_help_for(ctx, ctx.command)
+
+            for page in _help:
+                await ctx.send(page)
+
+    @change.command(name="playing")
+    @commands.check(repo.is_owner)
+    async def change_playing(self, ctx, *, playing: str):
+        """ Change playing status. """
+        try:
+            await self.bot.change_presence(
+                activity=discord.Game(type=0, name=playing),
+                status=discord.Status.online
+            )
+            dataIO.change_value("config.json", "playing", playing)
+            await ctx.send(f"Successfully changed playing status to **{playing}**")
+        except discord.InvalidArgument as err:
+            await ctx.send(err)
+        except Exception as e:
+            await ctx.send(e)
+
+    @change.command(name="username")
+    @commands.check(repo.is_owner)
+    async def change_username(self, ctx, *, name: str):
+        """ Change username. """
+        try:
+            await self.bot.user.edit(username=name)
+            await ctx.send(f"Successfully changed username to **{name}**")
+        except discord.HTTPException as err:
+            await ctx.send(err)
+
+    @change.command(name="nickname")
+    @commands.check(repo.is_owner)
+    async def change_nickname(self, ctx, *, name: str = None):
+        """ Change nickname. """
+        try:
+            await ctx.guild.me.edit(nick=name)
+            if name:
+                await ctx.send(f"Successfully changed nickname to **{name}**")
+            else:
+                await ctx.send("Successfully removed nickname")
+        except Exception as err:
+            await ctx.send(err)
+
+    @change.command(name="avatar")
+    @commands.check(repo.is_owner)
+    async def change_avatar(self, ctx, url: str = None):
+        """ Change avatar. """
+        if url is None and len(ctx.message.attachments) == 1:
+            url = ctx.message.attachments[0].url
+        else:
+            url = url.strip('<>')
+
+        try:
+            bio = await http.get(url, res_method="read")
+            await self.bot.user.edit(avatar=bio)
+            await ctx.send(f"Successfully changed the avatar. Currently using:\n{url}")
+        except aiohttp.InvalidURL:
+            await ctx.send("The URL is invalid...")
+        except discord.InvalidArgument:
+            await ctx.send("This URL does not contain a useable image")
+        except discord.HTTPException as err:
+            await ctx.send(err)
+
     @commands.command(aliases=['exec'])
     @commands.check(repo.is_owner)
     async def execute(self, ctx, *, text: str):
@@ -133,7 +201,7 @@ class Admin:
     async def whtest(self, ctx, whlink: str):
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(f'{whlink}', adapter=AsyncWebhookAdapter(session))
-            await webhook.send("owo has successfully booted, i think")
+            await webhook.send("hewwo?")
     
                 
 def setup(bot):
