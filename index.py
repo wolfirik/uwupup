fdbdbe415f92f6dff8f697e710ee4e5737d9472f
Skullbite <mawler12@gmail.com>
2018-05-10 12:55:47 -0400

    Update index.py

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ Made by AlexFlipnote
 
 print("-w- zzz...")
 
-bot = Bot(command_prefix=os.environ["PREFIX"], prefix=os.environ["PREFIX"], pm_help=True)
+bot = Bot(command_prefix=commands.when_mentioned_or("owo "), prefix=commands.when_mentioned_or("owo "), pm_help=True)
 
 for file in os.listdir("cogs"):
     if file.endswith(".py"):
