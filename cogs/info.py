9ee11da539b15f87841e7349afbc61790fdd33b2
Skullbite <mawler12@gmail.com>
2018-05-15 12:16:15 -0400

    Command counter

--- cogs/info.py
+++ cogs/info.py
@@ -65,6 +65,7 @@ class Information:
         embed.add_field(name="Dev", value=f"{owner}", inline=True)
         embed.add_field(name="Library", value="discord.py", inline=True)
         embed.add_field(name="Servers", value=len(ctx.bot.guilds), inline=True)
+        embed.add_field(name="Commands used", value=self.bot.counter["commands_ran"], inline=True)
         embed.add_field(name="RAM", value=f"{ramUsage:.2f} MB", inline=True)
 
         await ctx.send(content=f"ℹ About **{ctx.bot.user}**", embed=embed)
