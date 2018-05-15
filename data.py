46862960edfeeeab1640b8bd7d4cb78fe1a20610
Skullbite <mawler12@gmail.com>
2018-05-15 10:55:50 -0400

    Update data.py

--- data.py
+++ data.py
@@ -4,9 +4,11 @@ from discord.ext.commands import AutoShardedBot
 class Bot(AutoShardedBot):
     def __init__(self, *args, prefix=None, **kwargs):
         super().__init__(*args, **kwargs)
+        self.counter = Counter()
 
     async def on_message(self, msg):
         if not self.is_ready() or msg.author.bot or not permissions.can_send(msg):
             return
 
         await self.process_commands(msg)
+        self.counter["commands_ran"] += 1
