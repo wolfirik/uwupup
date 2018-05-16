f84b4ea08f3df8a8d5697b53811926cf51d89d0c
Skullbite <mawler12@gmail.com>
2018-05-16 13:42:52 -0400

    latency ping

--- cogs/info.py
+++ cogs/info.py
@@ -39,7 +39,7 @@ class Information:
         before = time.monotonic()
         message = await ctx.send("Pong")
         ping = (time.monotonic() - before) * 1000
-        await message.edit(content=f"Pong   |   {int(ping)}ms")
+        await message.edit(content=f"Pong `Edit:` {int(ping)}ms | `Bot speed:` {self.bot.latency}ms")
 
     @commands.command(aliases=['joinme', 'join', 'botinvite'])
     async def links(self, ctx):
