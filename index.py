c2384be634c9a28c1e8df34e5f0ba1a82cf987ac
Skullbite <mawler12@gmail.com>
2018-05-16 10:15:25 -0400

    mySql attempts

--- index.py
+++ index.py
@@ -17,6 +17,22 @@ print("-w- zzz...")
 
 bot = Bot(command_prefix=commands.when_mentioned_or("owo "), prefix=commands.when_mentioned_or("owo "), pm_help=True)
 
+def init_function(bot):
+	global cursor, engine, Session
+	if bot.dev_mode:
+		db = 'discord_dev'
+	elif bot.self_bot:
+		db = 'discord_self'
+	else:
+		db = 'discord'
+	engine = create_engine('mysql+pymysql://0:@localhost/{1}?charset=utf8mb4'.format(db), encoding='utf8')
+	session_factory = sessionmaker(bind=engine)
+	Session = scoped_session(session_factory)
+	bot.mysql = Object()
+	engine = bot.mysql.engine = engine
+	cursor = bot.mysql.cursor = bot.get_cursor
+
+
 for file in os.listdir("cogs"):
     if file.endswith(".py"):
         name = file[:-3]
