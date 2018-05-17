d3f0112696f89914412d612eedb8ca17cb3a1102
Skullbite <mawler12@gmail.com>
2018-05-17 09:06:21 -0400

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -305,7 +305,7 @@ class Admin:
     @commands.command()
     @commands.check(repo.is_owner)
     async def sqltest(self, ctx, *, command: str):
-        sql = f'INSERT INTO `test` (`message`) VALUES (%s)'
+        sql = f'INSERT INTO `test` (`message`) VALUES ({command})'
         self.c.execute(sql, (command))
         self.conn.commit()
         await ctx.send("saved {command} to sql")
