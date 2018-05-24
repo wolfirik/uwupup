4fbfa8b6f74c2f1f84b683fd003f679045ca485e
Skullbite <mawler12@gmail.com>
2018-05-24 11:05:39 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -84,11 +84,11 @@ class Fun_Commands:
 
         else:
             try:
-                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20-type:swf%20fur') #a lot more complex than the other apis
+                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20-type:swf%20fur')
                 r = r.json()
                 link = r[0].get('file_url')
             except:
-                ctx.send("I think e926 is being dumb.. try again later..")
+                return ctx.send("I think e926 is being dumb.. try again later..")
 
             hugge = discord.Embed(description=f"**{ctx.author.name} gave {user.name} a hug uwu**", color=0xd25e92)
             hugge.set_image(url=link)
