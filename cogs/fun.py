8520f8bebfad03bce7711e8fcf17ee5d4691c2db
Skullbite <mawler12@gmail.com>
2018-05-24 12:03:18 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -87,15 +87,16 @@ class Fun_Commands:
                 r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20-type:swf%20fur') 
                 r = r.json()
                 link = r[0].get('file_url')
+                thing = BytesIO(await http.get(link, res_method="read"))
             except:
                 return await ctx.send("I think e926 is being dumb.. try again later..")
 
             hugge = discord.Embed(description=f"**{ctx.author.name} gave {user.name} a hug uwu**", color=0xd25e92)
             hugge.set_image(url=link)
             try:
-                await ctx.send(embed=hugge)
+                await ctx.send(file=thing)
             except:
-                await ctx.send("aww i can't send embeds ;w;")
+                await ctx.send("something oofed.")
 
     @commands.command()
     @commands.is_nsfw() # TODO: Make a nsfw cog.
