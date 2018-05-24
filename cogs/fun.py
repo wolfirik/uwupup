b05f02bae2cbeb79c2bd38aadb9406f58de2be03
Skullbite <mawler12@gmail.com>
2018-05-24 15:48:05 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -62,7 +62,7 @@ class Fun_Commands:
     async def floof(self, ctx):
         """Posts a cute floof :3""" 
         try:
-            r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20-suggestive%20-type:swf%20fur') #a lot more complex than the other apis
+            r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20-suggestive%20-equine%20fur') #a lot more complex than the other apis
             r = r.json()
             link = r[0].get('file_url')
         except:
@@ -86,7 +86,7 @@ class Fun_Commands:
 
         else:
             try:
-                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20-type:swf%20fur') 
+                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20-equine%20fur') 
                 r = r.json()
                 link = r[0].get('file_url')
                 thing = BytesIO(await http.get(link, res_method="read"))
@@ -113,7 +113,7 @@ class Fun_Commands:
             await ctx.send(f"maybe someone other than your self {ctx.author.name}..?")
         else:
             try:
-                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20lick%20-type:swf%20duo') 
+                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20lick%20-equine%20duo') 
                 r = r.json()
                 link = r[0].get('file_url')
                 thing = BytesIO(await http.get(link, res_method="read"))
