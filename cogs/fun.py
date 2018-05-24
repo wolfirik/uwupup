9e8bab0abb66aa9f66e2cff4d40b8a7dd2144a7f
Skullbite <mawler12@gmail.com>
2018-05-24 11:22:37 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -84,7 +84,7 @@ class Fun_Commands:
 
         else:
             try:
-                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20-type:swf%20fur')
+                r = requests.get('https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20fur')
                 r = r.json()
                 hug = r[0].get('file_url')
             except:
