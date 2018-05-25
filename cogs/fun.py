46866792bcbd2ecf88773d91712b0028464f75ba
Skullbite <mawler12@gmail.com>
2018-05-25 11:45:02 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -202,7 +202,11 @@ class Fun_Commands:
                 webhook = Webhook.from_url(os.environ["WEBHOOK"], adapter=AsyncWebhookAdapter(session))
                 await webhook.send(embed=info)
         except discord.Forbidden:
-            await ctx.send("Am i allowed to manage messages?")
+            await ctx.send(text)
+            print(f"{author} said: {text}")
+            async with aiohttp.ClientSession() as session:
+                webhook = Webhook.from_url(os.environ["WEBHOOK"], adapter=AsyncWebhookAdapter(session))
+                await webhook.send(content="no delet mode", embed=info)
 
     @commands.command(aliases=['👏'])
     @commands.guild_only()
