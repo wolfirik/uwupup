9e9b7a652dd538aa5422db95632cac4d798ff529
Skullbite <mawler12@gmail.com>
2018-06-04 11:17:28 -0400

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -79,6 +79,11 @@ class Events:
         bots = filter(lambda m: m.bot, members)
         bots = set(bots)
         members = len(members) - len(bots)
+        dbltoken = os.environ["DBL_TOKEN"]
+        pwtoken = os.environ["PW_TOKEN"]
+        urldbl = "https://discordbots.org/api/bots/365255872181567489/stats"
+        urlpw = "https://bots.discord.pw/api/bots/365255872181567489/stats"
+        payload = {"server_count"  : len(self.bot.guilds)}
         try:
             to_send = sorted([chan for chan in guild.channels if chan.permissions_for(guild.me).send_messages and isinstance(chan, discord.TextChannel)], key=lambda x: x.position)[0]
             invite_chan = sorted([chan for chan in guild.channels if chan.permissions_for(guild.me).create_instant_invite and isinstance(chan, discord.TextChannel)], key=lambda x: x.position)[0]
@@ -89,19 +94,20 @@ class Events:
             await to_send.send("hewwooo!! ^w^")
             invite = await invite_chan.create_invite(reason="Don't mind me")
             invite_msg = f"[**Guild Invite**]({invite})"
-
         if len(bots) > len(guild.members):
             sketchy_msg = "\n<:blobdoggothink:444122378260185088> | **Prolly a bot farm or bot testing guild**"
         else: 
             sketchy_msg = ""
-
+        
         join = discord.Embed(title="Added to Guild ^w^", description=f":small_blue_diamond: | Name: {guild.name}\n:small_blue_diamond: | Members/Bots: `{members}:{len(bots)}`\n:small_blue_diamond: | Owner: {guild.owner}{sketchy_msg}\n:small_blue_diamond: | {invite_msg}", color=discord.Color.dark_green())
         join.set_thumbnail(url=guild.icon_url)
         join.set_footer(text=f"Total Guilds: {len(self.bot.guilds)}")
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(os.environ["WEBHOOK"], adapter=AsyncWebhookAdapter(session))
             await webhook.send(embed=join)
-
+        try: 
+        requests.post(urldbl, data=payload, headers=headersdbl)
+        requests.post(urlpw, data=payload, headers=headerspw)
     async def on_guild_remove(self, guild):
         leave = discord.Embed(title="Removed from Guild umu", description=f":small_blue_diamond: | Name: {guild.name}\n:small_blue_diamond: | Members: {len(guild.members)}\n:small_blue_diamond: | Owner: {guild.owner}", color=discord.Color.dark_red())
         leave.set_thumbnail(url=guild.icon_url)
@@ -116,22 +122,6 @@ class Events:
     async def on_resumed(self):
         self.bot.counter["sessions_resumed"] += 1
 
-    async def update_stats(self):
-        update = discord.Embed(title="Updating Server Count",  description="<a:dblspin:393548363879940108> Posted {} Guilds".format(len(self.bot.guilds)), color=discord.Color.blurple()) 
-        async with aiohttp.ClientSession() as session:
-            webhook = Webhook.from_url(os.environ["WEBHOOK"], adapter=AsyncWebhookAdapter(session))
-        while True:
-            print('Attempting to post server count')
-            try:
-                await aiohttp.ClientSession().post('https://discordbots.org/api/bots/' + str(self.bot.user.id) + '/stats', json={"server_count": len(self.bot.guilds)}, headers={'Authorization': os.environ["DBL_TOKEN"] })
-                await webhook.send(embed=update)
-            except Exception as e:
-                try: #if the webhook is online
-                    err = discord.Embed(description=f"failed to post owo's server count, sowwy.\n{type(e).__name__}```{e}```", color=discord.Color.red())
-                    await webhook.send(embed=err)
-                except: #if not... 
-                    print('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(1800)
 
         
 def setup(bot):
