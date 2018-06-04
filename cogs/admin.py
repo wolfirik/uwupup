bdd9ca580cd113a5b42b35f09b012cb947c55ca9
Skullbite <mawler12@gmail.com>
2018-06-04 15:21:29 -0400

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -350,7 +350,7 @@ class Admin:
     @commands.command()
     @commands.check(repo.is_owner)
     async def mongo(self, ctx, *, thing: str):
-        post_data = {}
+        post_data = { 'name' : "jeff" }
         headers = {'content-type': 'application/json'}
         r = requests.post(f'https://api.mlab.com/api/1/databases/{os.environ["DB_NAME"]}/collections/test?apiKey={os.environ["MLAB_KEY"]}', data=post_data, headers=headers)
         r = r.json()
