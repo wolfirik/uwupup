aebe8755ce7a1b4ba31ba7a330bd125f0d7bdf87
Stowe <672217@aacps.org>
2018-06-06 10:34:59 -0400

    s

--- cogs/test.py
+++ cogs/test.py
@@ -22,7 +22,7 @@ conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
 
 conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
       VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
-
+#
 conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
       VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
 
