6a784dd2a4a1c0b10821c987c32c3d39bbf60649
Stowe <672217@aacps.org>
2018-06-08 09:21:48 -0400

    Welcomer Support W.I.P

--- cogs/welcomersupport.py
+++ cogs/welcomersupport.py
@@ -11,11 +11,27 @@ class welcomersupport:
         auto-help = self.bot.get_channel(454308883964362763)
         if ctx.channel != auto-help:
             pass
+        elif ctx.author.bot:
+            return
         else:
             if message.content.startswith('help'):
-                help = discord.embed(title="Welcomer Support", description="`1.` How do i setup welcomer?\n"
-                                                                           "`2.` How do i enable/disable images and text?\n"
-                                                                           "`3.` How do i change image text?\n"
-                                                                           "`5.` How do i choose a welcoming channel?\n"
-                                                                           "`6.` How do i mention someone in the welcome message?")
-                                                                           
+                bd = ":small_blue_diamond:"
+                welcomer = self.bot.get_user(330416853971107840)
+                help = discord.embed(title="Welcomer Support", description=f"{bd} `1.` How do i setup welcomer?\n"
+                                                                           f"{bd} `2.` How do i enable/disable images and text?\n"
+                                                                           f"{bd} `3.` How do i change image text?\n"
+                                                                           f"{bd} `5.` How do i choose a welcoming channel?\n"
+                                                                           f"{bd} `6.` How do i mention someone in the welcome message?\n"
+                                                                           f"{bd} `7.` How do i change the background?\n"
+                                                                           f"{bd} `8.` How do i get the markdown?\n"
+                                                                           f"{bd} `9.` How do i set a custom image?\n"
+                                                                           f"{bd} `10.` How do i use gif backgrounds?\n"
+                                                                           f"{bd} `11.` How do i change the welcome colours?\n"
+                                                                           f"{bd} `12.` How do i change the prefix?\n"
+                                                                           f"{bd} `13.` My question isn't here\n"
+                                                                           "*send the number coressponding to your issue to get help*", color=discord.Color.blue())
+                help.set_footer(text="Thank You for choosing welcomer!", icon_url=welcomer.icon_url)
+                await auto-help.send(content=f"{ctx.author.mention}", embed=help)
+
+def setup(bot):
+    bot.add_cog(welcomersupport(bot))
