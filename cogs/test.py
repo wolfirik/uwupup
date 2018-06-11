eea1b8733309ab9337d101d95bc480ccfae6f22f
Stowe <672217@aacps.org>
2018-06-11 15:19:07 -0400

    l

--- cogs/test.py
+++ cogs/test.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 from utils import default, repo, http
 from io import BytesIO
-import PIL
+
 
 class sql_test_cog:
     def __init__(self, bot):
@@ -22,14 +22,6 @@ class sql_test_cog:
         await ctx.send("done")
         conn.close()
 
-    @commands.command()
-    @commands.check(repo.owner)
-    async def color(self, ctx):
-        bio = BytesIO(await http.get(ctx.author.avatar_url.replace("webp", "png"), res_method="read"))
-        img = await PIL.ImageOps.colorize(bio, rgb(255,0,0), rgb(153,0,0))
-        final = BytesIO()
-        img = img.save(final, 'png')
-        await ctx.send(file=discord.File(img, filename="color.png"))
 
 def setup(bot):
     bot.add_cog(sql_test_cog(bot))
