cb3647eeefe112d64e4224e5b3386f1b35e03ee4
Skullbite <mawler12@gmail.com>
2018-07-28 02:21:35 -0400

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -139,12 +139,13 @@ class Information:
             base = requests.get(f"https://discordbots.org/api/bots/{bot.id}").json()
             emote = self.bot.get_emoji(393548363879940108)
             prefix = base.get("prefix")
+            tags = ", ".join(base.get("tags"))
             desc = base.get("shortdesc")
             owners = list(base.get("owners"))
             
             for owner in owners:
                 owners = self.bot.get_user(int(owner))
-            m = discord.Embed(description=f"```{desc}```")
+            m = discord.Embed(description=f"```{desc}```\n\n Tags: {tags}", color=self.color)
             m.set_footer(text=f"Primary Owner: {owners}", icon_url=owners.avatar_url)
             m.set_author(name=f"DBL stats for {bot}", icon_url=emote.url)
             m.set_thumbnail(url=bot.avatar_url)
