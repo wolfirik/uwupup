9843c929b58673a0599a86afccd32deb8481ef2c
Skullbite <mawler12@gmail.com>
2018-07-28 03:27:32 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -115,12 +115,10 @@ class Fun_Commands:
 
         else:
             try:
-                async with aiohttp.ClientSession(headers={'User-Agent': 'Chrome/60.0.3112.113'}) as session:
-                    async with session.get(f'https://e926.net/post/index.json?limit=1&tags=cute%20order:random%20hug%20-equine%20fur') as get:
-                        resp = await get.json()
-                        hugge = discord.Embed(description=f"**{ctx.author.name} gave {user.name} a hug uwu**", color=0xd25e92)
-                        hugge.set_image(url=resp['file_url'])
-                        await ctx.send(embed=hugge)
+                r = requests.get("https://sheri.fun/api/v1/img/hug", headers={"key": os.environ["MURR"]}).json().get("url")
+                hugge = discord.Embed(description=f"**{ctx.author.name} gave {user.name} a hug uwu**", color=0xd25e92)
+                hugge.set_image(url=r)
+                await ctx.send(embed=hugge)
             except Exception as e:
                 return await ctx.send(e)
             try:
@@ -226,8 +224,9 @@ class Fun_Commands:
                 async with aiohttp.ClientSession(headers={'User-Agent': 'Chrome/60.0.3112.113'}) as session:
                     async with session.get(f'https://e926.net/post/index.json?limit=1&tags=kiss%20cute%20fur%20-equine%20order:random') as get:
                         resp = await get.json()
+                        r = requests.get("https://sheri.fun/api/v1/img/cuddles", headers={"key": os.environ["MURR"]}).json().get("url")
                         kiss = discord.Embed(description=f"**{ctx.author.name} kissed {user.name} \❤w\❤**", color=0xe91e63)
-                        kiss.set_image(url=resp['file_url'])
+                        kiss.set_image(url=r)
                         await ctx.send(embed=kiss)
             except:
                 return await ctx.send("I think e926 is being dumb.. try again later..")
