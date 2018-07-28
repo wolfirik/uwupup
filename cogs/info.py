b60da383869086c314bfe1fbbe00a4a3c16d9b2e
Skullbite <mawler12@gmail.com>
2018-07-28 01:52:01 -0400

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -137,13 +137,15 @@ class Information:
             pass
         else:
             base = requests.get(f"https://discordbots.org/api/bots/{bot.id}").json()
-            
+            emote = self.bot.get_emoji(338808864352763904)
             prefix = base.get("prefix")
             owners = list(base.get("owners"))
+            
             for owner in owners:
                 owners = self.bot.get_user(int(owner))
             m = discord.Embed(description="`quality place holder`")
             m.set_footer(text=f"Primary Owner: {owners}", icon_url=owners.avatar_url)
+            m.set_author(name=f"DBL stats for {bot}", icon_url=emote.url)
             await ctx.send(content=prefix, embed=m)
             
                 
