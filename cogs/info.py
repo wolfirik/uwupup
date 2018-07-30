66cbd14eb6bb31aa06b7a6103874acaa4b79c3ac
Skullbite <mawler12@gmail.com>
2018-07-30 15:07:19 -0400

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -109,11 +109,11 @@ class Information:
     @commands.group()
     async def dbl(self, ctx):
         if ctx.invoked_subcommand is None:
-        emote = self.bot.get_emoji(393548363879940108)
+            emote = self.bot.get_emoji(393548363879940108)
 
-        r = discord.Embed(description="Use `uwu dbl info/i` to get an embed with a bots info or `uwu dbl widget/w` to generate a widget", color=self.color)
-        r.set_author(name="DBL Menu", icon_url=emote.url)
-        await ctx.send(embed=r)
+            r = discord.Embed(description="Use `uwu dbl info/i` to get an embed with a bots info or `uwu dbl widget/w` to generate a widget", color=self.color)
+            r.set_author(name="DBL Menu", icon_url=emote.url)
+            await ctx.send(embed=r)
                 
     @dbl.command(name="widget", aliases=["w"])
     async def dbl_widget(self, ctx, botto: discord.Member):
