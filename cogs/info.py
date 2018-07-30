dd0f528c860bd60846923887c0b69623a8de92fd
Skullbite <mawler12@gmail.com>
2018-07-30 15:32:16 -0400

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -163,16 +163,16 @@ class Information:
                 points = base.get("points")
                 lib = base.get("lib")
                 desc = base.get("shortdesc")
-            owners = list(base.get("owners"))
-            link = f"https://discordbots.org/bot/{bot.id}"
+                owners = list(base.get("owners"))
+                link = f"https://discordbots.org/bot/{bot.id}"
             
-            for owner in owners:
-                owners = self.bot.get_user(int(owner))
-            m = discord.Embed(description=f"```{desc}```\n\nTotal Votes: {points}\nLib: {lib}\nPrefix: {prefix}\nTags: {tags}\nCertified? `{cert}`\n\n[{bot.name}'s DBL Page]({link})", color=self.color)
-           # m.set_footer(text=f"Primary Owner: {owners}", icon_url=owners.avatar_url)
-            m.set_author(name=f"DBL stats for {bot}", icon_url=emote.url)
-            m.set_thumbnail(url=bot.avatar_url)
-            await ctx.send(embed=m)
+                for owner in owners:
+                    owners = self.bot.get_user(int(owner))
+                m = discord.Embed(description=f"```{desc}```\n\nTotal Votes: {points}\nLib: {lib}\nPrefix: {prefix}\nTags: {tags}\nCertified? `{cert}`\n\n[{bot.name}'s DBL Page]({link})", color=self.color)
+               # m.set_footer(text=f"Primary Owner: {owners}", icon_url=owners.avatar_url)
+                m.set_author(name=f"DBL stats for {bot}", icon_url=emote.url)
+                m.set_thumbnail(url=bot.avatar_url)
+                await ctx.send(embed=m)
             
                 
     @commands.command(aliases=["lc"])
