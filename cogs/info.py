f30a17bd2bd774a1c128fb133b77dc57e702caa1
Skullbite <mawler12@gmail.com>
2018-08-02 23:27:39 -0400

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -252,7 +252,7 @@ class Information:
                 if status in 'streaming':
                     statuss = 4
                 botto = f"{bot.name}".replace(" ", "%20")
-                link = f"http://dblw.glitch.me/widget?name={botto}&server_count={guilds}&votes={points}&owner={owner_name}&status={statuss}&avatar={bot.id}|{bot.avatar}&certified={cert}"
+                link = f"http://172.96.162.194:4006/widget?name={botto}&server_count={guilds}&votes={points}&owner={owner_name}&status={statuss}&avatar={bot.id}|{bot.avatar}&certified={cert}"
                 if name.endswith("'s"):
                     name = name
                 elif name.endswith("'"):
