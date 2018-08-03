21e3f86fc8a07afc641513d6b065b94ab9deef8b
Skullbite <mawler12@gmail.com>
2018-08-03 18:19:28 -0400

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -205,14 +205,14 @@ class Information:
                 invite = base.get("invite")
                 github = base.get("github")
                 if github == "":
-                    github = "No Github"
+                    ggithub = "No Github"
                 else:
-                    github = github
+                    ggithub = github
                 website = base.get("website")
                 if website == "":
-                    website = "No website"
+                    wwebsite = "No website"
                 else:
-                    website = website
+                    wwebsite = website
                 owners = list(base.get("owners"))
                 servers = guilds.get("server_count")
                 shards = guilds.get("shard_count")
@@ -220,7 +220,7 @@ class Information:
             
                 for owner in owners:
                     owners = self.bot.get_user(int(owner))
-                m = discord.Embed(description=f"```{desc}```\n\nTotal Votes: {points}\nLib: {lib}\nPrefix: {prefix}\nTags: {tags}\nCertified? `{cert}`\nPosted Guild Count: {servers}\nPosted Shard Count: {shards}\n\n[{name} DBL Page]({link}) | [Invite]({invite}) | [Github]({github}) | [Website]({website})", color=self.color)
+                m = discord.Embed(description=f"```{desc}```\n\nTotal Votes: {points}\nLib: {lib}\nPrefix: {prefix}\nTags: {tags}\nCertified? `{cert}`\nPosted Guild Count: {servers}\nPosted Shard Count: {shards}\n\n[DBL Page]({link}) | [Invite]({invite}) | [Github]({ggithub}) | [Website]({wwebsite})", color=self.color)
                # m.set_footer(text=f"Primary Owner: {owners}", icon_url=owners.avatar_url)
                 m.set_author(name=f"DBL stats for {bot}", icon_url=emote.url)
                 m.set_thumbnail(url=bot.avatar_url)
