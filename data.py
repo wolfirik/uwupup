ecca54a30d7c658fca7d9261872b6ad33aeafad3
Skullbite <mawler12@gmail.com>
2018-08-03 00:34:32 -0400

    Update data.py

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ class Bot(AutoShardedBot):
     def __init__(self, *args, prefix=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.counter = Counter()
+        self.color = 0x254d16
 
     async def on_message(self, msg):
         if not self.is_ready() or msg.author.bot or not permissions.can_send(msg):
