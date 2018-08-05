f32efe564ef1e607f956cdb67ce2ecd3e1b7f3f9
Skullbite <mawler12@gmail.com>
2018-08-05 11:13:28 -0400

    Update discord.py

--- cogs/discord.py
+++ cogs/discord.py
@@ -19,9 +19,8 @@ class Discord_Info:
         avatar = avatar.replace("webp?size=2048", "png")
         thing = BytesIO(await http.get(avatar, res_method="read"))
         color = ColorThief(thing)
-        color = '0x%02x%02x%02x' % color.get_color(quality=1)
-        hex_result = "".join([format(val, '02X') for val in color])
-        embed = discord.Embed(colour=hex_result)
+        color = color.get_color(quality=10)
+        embed = discord.Embed(colour=discord.Color.from_rgb(*color))
         embed.description = f"Avatar to **{user.name}**\nClick [here]({avatar}) to get image"
         
         embed.set_image(url=avatar)
